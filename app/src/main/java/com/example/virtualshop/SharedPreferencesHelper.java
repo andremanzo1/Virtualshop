1b664c7d208242d913f5e2da850ce146bda14614
Andre Manzo <drecollege1@gmail.com>
2023-11-18 00:05:34 -0800

    checked on spacing reorganized

--- app/src/main/java/com/example/virtualshop/SharedPreferencesHelper.java
+++ app/src/main/java/com/example/virtualshop/SharedPreferencesHelper.java
@@ -4,13 +4,11 @@ import android.content.SharedPreferences;
 public class SharedPreferencesHelper {
     private static final String PREFS_NAME = "MyPrefsFile";
     private static final String KEY_IS_LOGGED_IN = "is_logged_in";
-
     public static void setLoggedIn(Context context, boolean isLoggedIn) {
         SharedPreferences.Editor editor = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE).edit();
         editor.putBoolean(KEY_IS_LOGGED_IN, isLoggedIn);
         editor.apply();
     }
-
     public static boolean isLoggedIn(Context context) {
         SharedPreferences prefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
         return prefs.getBoolean(KEY_IS_LOGGED_IN, false);
