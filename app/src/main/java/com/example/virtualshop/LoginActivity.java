9b29905a33ee363e1f86d0e69bf92dbe883e2bc8
Andre Manzo <drecollege1@gmail.com>
2023-11-18 00:04:49 -0800

    added and removed files

---
+++ app/src/main/java/com/example/virtualshop/LoginActivity.java
@@ -0,0 +1,26 @@
+package com.example.virtualshop;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+public class LoginActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        EditText usernameEditText = findViewById(R.id.usernameEditText);
+        EditText passwordEditText = findViewById(R.id.passwordEditText);
+        Button loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Implement logic to check username and password, and store in Room database
+                // If login is successful, set the user as logged in in SharedPreferences
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/virtualshop/MainActivity.java
+++ app/src/main/java/com/example/virtualshop/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.virtualshop;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.virtualshop.SharedPreferencesHelper;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Check if the user is already logged in
+        if (SharedPreferencesHelper.isLoggedIn(this)) {
+            startLandingPage();
+            return;
+        }
+
+        // Setup login and create account buttons
+        Button loginButton = findViewById(R.id.loginButton);
+        Button createAccountButton = findViewById(R.id.createAccountButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
+
+        createAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Implement logic to create an account
+            }
+        });
+    }
+
+    private void startLandingPage() {
+        startActivity(new Intent(MainActivity.this, LandingPageActivity.class));
+        finish();
     }
 }
